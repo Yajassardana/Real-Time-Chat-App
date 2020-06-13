541479e49fff7deef26aa21f52a6311f8bc96cc8
Yajas Sardana <yajassardana@gmail.com>
2020-06-14 04:54:42 +0530

    styling improvements

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -8,8 +8,6 @@ const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
       <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
     </div>
     {
       users
