07d6d14bffec75b99d1dd79fa2a43f35aba0fa66
Yajas Sardana <yajassardana@gmail.com>
2020-06-23 02:14:39 +0530

    prepare to deploy

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
 	const [users, setUsers] = useState('');
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
-	const ENDPOINT = 'localhost:5000';
+	const ENDPOINT = 'https://chatapp-real-time.herokuapp.com/';
 
 	useEffect(() => {
 		const { name, room } = queryString.parse(location.search);
